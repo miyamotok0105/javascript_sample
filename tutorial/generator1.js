31c1b3028524fe7330fb6596c0ef801d59591818
miyamoto <miyamotok0105@gmail.com>
2018-07-22 18:01:06 +0900

    Add gen

---
+++ tutorial/generator1.js
@@ -0,0 +1,17 @@
+
+function* idMaker() {
+  var index = 0;
+  while(true)
+    yield index++;
+}
+
+var gen = idMaker();
+
+console.log(gen.next().value); // 0
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 2
+
+for (let value of gen) { 
+    console.log(value);
+    break;
+}
