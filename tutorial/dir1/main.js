adc70ec6a531cedd59249c4ba647388dc7c3685f
miyamoto <miyamotok0105@gmail.com>
2018-09-26 15:48:16 +0900

    Add export module example

---
+++ tutorial/dir1/main.js
@@ -0,0 +1,9 @@
+// main.js
+var weather = require('weather-module');
+weather.today();
+weather.yesterday();
+
+
+var weather2 = require('weather-module');
+weather2.today();
+weather2.yesterday();
---
+++ tutorial/dir2/main.js
@@ -0,0 +1,7 @@
+// main.js
+var weather_module = require('weather-module');
+weather_module.weather.today();
+weather_module.weather.yesterday();
+
+weather_module.weather2.today();
+weather_module.weather2.yesterday();
